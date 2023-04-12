18d158d932e349c00e653a1bf64fb616085413c8
dedvadja <psill@list.ru>
2023-04-12 11:04:14 +0300

    Changed the HTML for the site

--- DjangoFolder/mysite/polls/views.py
+++ DjangoFolder/mysite/polls/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
